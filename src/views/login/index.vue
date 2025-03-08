eb01faeec2d36ed5f9f7f588ccb5ee59e0ac45bc
home <897601562@qq.com>
2025-03-08 18:16:44 +0800

    20250308_home_first

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,57 @@
 <template>
-    <div>
-        <el-row class="mb-4">
-            <el-button>Default</el-button>
-            <el-button type="primary">Primary</el-button>
-            <el-button type="success">Success</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-        </el-row>
-    </div>
+  <el-row class="login-box" justify="center" :align="'middle'">
+    <el-card style="max-width: 480px;">
+        <template #header>
+            <div>
+                <img width="100%" :src="imgUrl" alt="">
+            </div>
+        </template>
+        <div class="login-switch">
+            <el-link type="primary" @click="()=> {isHaveNum = !isHaveNum}">{{ isHaveNum ? '注册账号' : '返回登陆'}}</el-link>
+        </div>
+        <el-form :model="loginForm" >
+            <el-form-item prop="userName">
+                <el-input v-model="loginForm.userName" placeholder="请输入用户名" prefix-icon="UserFilled" />
+            </el-form-item>
+            <el-form-item prop="passWord">
+                <el-input v-model="loginForm.passWord" placeholder="请输入密码" prefix-icon="Lock" />
+            </el-form-item>
+            <el-form-item prop="validCode">
+                <el-input v-model="loginForm.validCode" placeholder="请输入验证码" prefix-icon="UserFilled" />
+            </el-form-item>
+        </el-form>
+    </el-card>
+  </el-row>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue';
+
+const imgUrl = new URL('../../../public/login-header.png', import.meta.url).href
+
+const isHaveNum = ref(true)
+const loginTypeClick = () => {
+    isHaveNum = !isHaveNum
+}
+
+const loginForm = reactive({
+    userName: '',
+    passWord: '',
+    validCode: ''
+})
+</script>
+
+<style lang="less" scoped>
+.login-box {
+    height: 100%;
+    .login-switch {
+        text-align: right;
+        margin-bottom: 10px;
+    }
+    .login-input {
+        margin-top: 10px;
+    }
+}
+
+
+</style>
